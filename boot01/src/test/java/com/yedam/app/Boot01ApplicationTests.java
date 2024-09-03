eddf2f102660222008ff7445c56b15daa7417d9e
coon3300 <coon3300@gmail.com>
2024-09-03 12:41:22 +0900

    junit

--- boot01/src/test/java/com/yedam/app/Boot01ApplicationTests.java
+++ boot01/src/test/java/com/yedam/app/Boot01ApplicationTests.java
@@ -1,13 +1,27 @@
 package com.yedam.app;
 
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.List;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import com.yedam.app.emp.mapper.EmpMapper;
+import com.yedam.app.emp.serviece.EmpVO;
 
 @SpringBootTest
 class Boot01ApplicationTests {
-
+	
+	@Autowired // 필드 주입
+	private EmpMapper empMapper;
 	@Test
-	void contextLoads() {
+	void empList() {
+//		assertEquals(empMapper, null);
+//		assertNotNull(empMapper);
+		List<EmpVO> list = empMapper.selectEmpAllList();
+		assertTrue(!list.isEmpty());
 	}
 
 }
