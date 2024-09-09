58f1b5991737573a4f9eeeabe8457def8926e349
coon3300 <coon3300@gmail.com>
2024-09-09 15:33:38 +0900

    file upload

---
+++ boot01/src/main/java/com/yedam/app/web/config/WebMVCConfig.java
@@ -0,0 +1,24 @@
+package com.yedam.app.web.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebMVCConfig implements WebMvcConfigurer{
+	
+	@Value("${file.upload.path}") // 환경변수 및 Properties 파일 Read
+	private String uploadPath;
+
+	// 리소스 핸들링
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		
+		//WebMvcConfigurer.super.addResourceHandlers(registry);
+		registry
+		.addResourceHandler("/imgs/**") // URL ** => 하위 폴더
+		.addResourceLocations("file:///" + uploadPath); // 실제 경로
+	}
+	
+}
