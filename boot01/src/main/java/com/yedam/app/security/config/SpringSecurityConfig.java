8d492d4da7cb1c1bf1afbe97f1b1b36dbf1c7901
coon3300 <coon3300@gmail.com>
2024-09-09 17:52:03 +0900

    security

---
+++ boot01/src/main/java/com/yedam/app/security/config/SpringSecurityConfig.java
@@ -0,0 +1,59 @@
+package com.yedam.app.security.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@EnableWebSecurity
+@Configuration
+public class SpringSecurityConfig {
+	@Bean // 비밀번호 암호화
+	PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder(); //BCrypt 단방향 해시 알고리즘
+	}
+	
+	@Bean // 메모리상 인증정보 등록 => 테스트 전용
+	InMemoryUserDetailsManager inMemoryUserDetailsService() {
+		UserDetails user = 	User.builder()
+								.username("user1")
+								.password(passwordEncoder().encode("1234"))
+								.roles("USER") // ROLE_USER : ROLE_ 자동 추가
+//								.authorities("ROLE_USER")
+								.build();
+		
+		UserDetails admin = 	User.builder()
+									.username("admin1")
+									.password(passwordEncoder().encode("1234"))
+//									.roles("ADMIN") // ROLE_USER : ROLE_ 자동 추가
+									.authorities("ROLE_ADMIN")
+									.build();
+		
+		return new InMemoryUserDetailsManager(user, admin);
+	}
+	
+	// 인증 및 인가
+	@Bean
+	SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+		// 적용될 Security 설정
+		// => URI에 적용될 권한
+		http.authorizeHttpRequests(authorize -> authorize
+				.requestMatchers("/", "all").permitAll()
+//				.requestMatchers("/user/**").hasRole("USER")
+				.requestMatchers("/user/**").hasAnyRole("USER", "ADMIN")
+				.requestMatchers("/admin/**").hasAuthority("ROLE_ADMIN")
+				.anyRequest().authenticated() // 위 경로 제외 일괄 인증처리
+		)
+		.formLogin(formlogin -> formlogin.defaultSuccessUrl("/all"))
+		.logout(logout -> logout.logoutSuccessUrl("/all"));
+		
+		
+		return http.build();
+	}
+}
