51a34c43783084a6e5925507819e13ed16f6d934
coon3300 <coon3300@gmail.com>
2024-10-30 10:28:29 +0900

    Delete sjc directory

--- sjc/src/main/java/com/sjc/app/security/mapper/UserMapper.java
+++
@@ -1,7 +0,0 @@
-package com.sjc.app.security.mapper;
-
-import com.sjc.app.security.service.UserVO;
-
-public interface UserMapper {
-	public UserVO getUserInfo(String username);
-}
--- sjc/src/test/java/com/yedam/app/SjcApplicationTests.java
+++
@@ -1,8 +0,0 @@
-package com.yedam.app;
-
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class SjcApplicationTests {
-	
-}
